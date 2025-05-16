fb462b137181225e312bc9dc29b6ea215e7e017b
MKode312 <mark_kork@icloud.com>
2025-05-16 19:19:01 +0500

    Update stack.go

--- internal/utils/agent/calculation/stack.go
+++ internal/utils/agent/calculation/stack.go
@@ -1,16 +1,13 @@
 package calculation
 
-// Stack наш стек для работы с постфиксами
 type Stack struct {
 	Items []float64
 }
 
-// Push пушит числа в стек
 func (s *Stack) Push(item float64) {
 	s.Items = append(s.Items, item)
 }
 
-// Pop забирает (буквально) элемент со стека
 func (s *Stack) Pop() float64 {
 	item := s.Items[len(s.Items)-1]
 	s.Items = s.Items[:len(s.Items)-1]
