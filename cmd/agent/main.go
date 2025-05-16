22617dc66f764e5fd619b3ec16b9e067584a318e
MKode312 <mark_kork@icloud.com>
2025-05-16 19:15:18 +0500

    Update main.go

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -2,9 +2,6 @@ package main
 
 import "calculator_go/internal/grpc/agent"
 
-// Agent - the calculating server, which parses expression ->
-// calculates the answer -> returns result to Orchestrator
-
 func main() {
 	agent.RunAgentServer()
 }
